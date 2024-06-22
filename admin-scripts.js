f7da7698157a8d90824a54814c3f2745b2cec47f
Kiodd <rossi.06062007@gmail.com>
2024-06-22 11:30:56 -0400

    test

--- admin-scripts.js
+++ admin-scripts.js
@@ -1,27 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const username = localStorage.getItem('username');
-  if (username) {
-    document.getElementById('welcome-message').textContent = `Bienvenido, ${username}`;
-  }
-
-  document.getElementById('card-ingresar-propiedades').addEventListener('click', function() {
-    const subCards = document.getElementById('sub-cards-propiedades');
-    subCards.classList.toggle('show');
-    if (subCards.classList.contains('show')) {
-      subCards.style.maxHeight = subCards.scrollHeight + 'px';
-    } else {
-      subCards.style.maxHeight = '0';
+    const username = localStorage.getItem('username');
+    if (username) {
+        document.getElementById('welcome-message').textContent = `Bienvenido, ${username}`;
     }
-  });
 
-  document.getElementById('card-ingresar-captacion').addEventListener('click', function() {
-    const subCards = document.getElementById('sub-cards-captacion');
-    subCards.classList.toggle('show');
-    if (subCards.classList.contains('show')) {
-      subCards.style.maxHeight = subCards.scrollHeight + 'px';
-    } else {
-      subCards.style.maxHeight = '0';
+    // Función para manejar el clic en las tarjetas principales
+    function toggleSubCards(cardId, subCardsId) {
+        const card = document.getElementById(cardId);
+        const subCards = document.getElementById(subCardsId);
+
+        card.addEventListener('click', function() {
+            subCards.classList.toggle('show');
+            if (subCards.classList.contains('show')) {
+                subCards.style.maxHeight = subCards.scrollHeight + 'px';
+            } else {
+                subCards.style.maxHeight = '0';
+            }
+        });
     }
-  });
+
+    // Manejar el clic en la tarjeta "Ingresar Propiedades"
+    toggleSubCards('card-ingresar-propiedades', 'sub-cards-propiedades');
+
+    // Manejar el clic en la tarjeta "Ingresar Captación"
+    toggleSubCards('card-ingresar-captacion', 'sub-cards-captacion');
+
+    // Manejar el clic en la tarjeta "Ver Captaciones"
+    toggleSubCards('card-ver-captaciones', 'sub-cards-ver-captaciones');
 });
-
